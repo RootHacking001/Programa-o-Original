fbee1447e67d8697ccfee9ef3fc429982ae868f5
RootHacking001 <explosaozica@gmail.com>
2021-04-12 15:42:40 -0200

    :package: menu sidebar

---
+++ script.js
@@ -0,0 +1,5 @@
+var paypal = document.querySelector(".paypal");
+
+paypal.addEventListener("click", function() {
+            var sidebar = document.querySelector(".container").classList.toggle("show-menu");
+        }
